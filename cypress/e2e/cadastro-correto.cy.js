b9ed8e082dc4523f33553a91345b16256e6ff9b7
kauaribeiroqa <kauaribeirodev@outlook.com>
2026-08-06 21:33:00 -0300

    ajustando seletores

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,11 +1,11 @@
 describe('Página de Cadastro', () => {
   it('deve cadastrar um novo usuário com sucesso', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a', 'Cadastrar').click();
-    cy.get('input[name="nome"]').type('Cristiano Ronaldo');
-    cy.get('input[name="email"]').type('cristiano.ronaldo12@email.com');
-    cy.get('input[name="password"]').type('Cristiano123');
-    cy.get('input[name="confirm_password"]').type('Cristiano123');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Cristiano Ronaldo');
+    cy.get('[data-test="input-email"]').type('cristiano.ronaldo12@email.com');
+    cy.get('[data-test="input-password"]').type('Cristiano123');
+    cy.get('[data-test="input-confirm-password"]').type('Cristiano123');
     cy.contains('button', 'Cadastrar').click();
   })
 })
